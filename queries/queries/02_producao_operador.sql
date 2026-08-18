8fe559a36acb9a4dd9f9dd2ae2278e17d87496f5
gabrielnasc17 <gabrielluizna@gmail.com>
2026-08-18 18:53:36 -0300

    Delete queries/queries/02_producao_operador.sql

--- queries/queries/02_producao_operador.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    o.nome AS operador,
-    SUM(p.quantidade_produzida) AS producao_total
-FROM producao_diaria p
-JOIN operadores o
-    ON p.id_operador = o.id_operador
-GROUP BY o.nome
-ORDER BY producao_total DESC;
